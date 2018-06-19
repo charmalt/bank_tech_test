6223d8b7783c4df9416f2156acef52c05d205182
Charlene <charlenechetcuti@gmail.com>
2018-06-19 02:02:50 +0100

    add transaction history test

--- src/test/java/bank/TransactionHistoryTest.java
+++ src/test/java/bank/TransactionHistoryTest.java
@@ -1,4 +1,24 @@
 package bank;
 
-public class TransactionHistoryTest {
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import java.util.ArrayList;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+
+class TransactionHistoryTest {
+
+    private TransactionHistoryTest history;
+
+    @BeforeEach
+    void setUp() {
+       history = new TransactionHistoryTest();
+    }
+
+    @Test
+    void hasNoTransactionsInitially() {
+        assertEquals(new ArrayList<Transaction>(), history.getTransactions());
+    }
+
 }
