113739e03f73d17c7d4c07eb425135a200cdcaf0
Charlene <charlenechetcuti@gmail.com>
2018-06-19 01:59:55 +0100

    mock transaction class

--- src/test/java/bank/AccountTest.java
+++ src/test/java/bank/AccountTest.java
@@ -4,10 +4,19 @@ import org.hamcrest.collection.IsEmptyCollection;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.TestInfo;
+import org.junit.runner.RunWith;
+import org.mockito.Mock;
+import org.mockito.Mockito;
+import org.mockito.Spy;
+import org.mockito.runners.MockitoJUnitRunner;
 
 import static org.hamcrest.MatcherAssert.assertThat;
 import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.mockito.Matchers.anyInt;
+import static org.mockito.Matchers.anyString;
 
+
+@RunWith(MockitoJUnitRunner.class)
 class AccountTest {
 
     private Account account;
@@ -23,14 +32,6 @@ class AccountTest {
     void newAccountHasDefaultZeroBalance(){
 
         assertEquals(account.getBalance(), 0);
-    }
-
-    @Test
-    void setAccountBalance(){
-
-        account.setBalance(100);
-
-        assertEquals(account.getBalance(), 100);
     }
 
     @Test
@@ -56,10 +57,29 @@ class AccountTest {
         assertEquals(account.getBalance(), 0);
     }
 
-    @Test
-    void saveNewTransaction(){
+    @Mock
+    private Transaction mockTransaction;
 
-        account.deposit(100, "12/01/2018");
+    @Spy
+    private Account accountSpy;
+
+    @Test
+    void makeNewTransaction(){
+
+        mockTransaction = Mockito.mock(Transaction.class);
+        accountSpy = Mockito.spy(new Account());
+        accountSpy.deposit(100, "12/01/2018");
+
+        Mockito.doReturn(mockTransaction).when(accountSpy).makeTransaction( anyString(), anyInt(),
+                anyInt(), anyInt());
+
+        assertEquals(mockTransaction, accountSpy.makeTransaction( "Hello",12,
+                12, 12));
+
+        System.out.println(accountSpy.getTransactions());
+
+        System.out.println(accountSpy.makeTransaction( "Hello",12,
+                12, 12));
 
     }
 
