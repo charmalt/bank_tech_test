5c83231428a8dcb438ad6553acb42a3c70351d41
Charlene <charlenechetcuti@gmail.com>
2018-06-19 18:14:33 +0100

    change statement formatting

--- src/main/java/bank/TransactionHistory.java
+++ src/main/java/bank/TransactionHistory.java
@@ -1,25 +1,41 @@
 package bank;
 
 import java.util.ArrayList;
+import java.util.Collections;
 
 class TransactionHistory {
 
     ArrayList<Transaction> transactions;
 
     TransactionHistory() {
+
         this.transactions = new ArrayList<>();
     }
 
     void addTransaction(Transaction transaction) {
+
         transactions.add(transaction);
     }
 
     void printStatement(){
 
-        System.out.printf("%10s %10s %10s %10s", "Date", "Withdrawal", "Deposit", "Balance");
+        System.out.printf("%4s || %6s || %5s || %7s", "date", "credit", "debit", "balance");
         System.out.println();
-        for (Transaction transaction : this.transactions) {
-            System.out.printf("%10s %10d %10d %10d", transaction.date, transaction.withdrawal, transaction.deposit, transaction.balance);
+
+        ArrayList<Transaction> transactions1 = this.transactions;
+
+        for (int i = transactions1.size() - 1; i >= 0 ; i--) {
+
+            Transaction transaction = transactions1.get(i);
+
+            String formatter = (transaction.withdrawal == 0.0) ? "%10s || %4.2f || || %4.2f" : "%10s || || %4.2f || %4.2f";
+
+            if (transaction.withdrawal == 0) {
+                System.out.printf(formatter, transaction.date, transaction.deposit, transaction.balance);
+            } else {
+                System.out.printf(formatter, transaction.date, transaction.withdrawal, transaction.balance);
+            }
+
             System.out.println();
         }
 
