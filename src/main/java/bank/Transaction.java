534cc2df1bed7418ff2c56bba7dd8d2126a57bbb
Charlene <charlenechetcuti@gmail.com>
2018-06-19 02:02:32 +0100

    refactor transaction class

--- src/main/java/bank/Transaction.java
+++ src/main/java/bank/Transaction.java
@@ -5,13 +5,12 @@ class Transaction {
     int deposit, withdrawal, balance;
     String date;
 
-    Transaction(String date, int deposit, int withdrawal, int balance){
+    Transaction(String date, int withdrawal, int deposit, int balance){
 
         this.date = date;
         this.deposit = deposit;
         this.withdrawal = withdrawal;
         this.balance = balance;
     }
-
 
 }
