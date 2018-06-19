9d3dff246be53620bb1ebae1759321ea256fd9ae
Charlene <charlenechetcuti@gmail.com>
2018-06-19 11:58:46 +0100

    mock Transaction in Transaction History Test

--- src/test/java/bank/TransactionHistoryTest.java
+++ src/test/java/bank/TransactionHistoryTest.java
@@ -2,6 +2,9 @@ package bank;
 
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.TestInfo;
+import org.mockito.Mock;
+import org.mockito.Mockito;
 
 import java.util.ArrayList;
 
@@ -12,14 +15,25 @@ class TransactionHistoryTest {
 
     private TransactionHistory history;
 
+    @Mock
+    private Transaction mockTransaction;
+
     @BeforeEach
-    void setUp() {
+    void init(TestInfo testInfo) {
         history = new TransactionHistory();
+        mockTransaction = Mockito.mock(Transaction.class);
+        System.out.println("Start..." + testInfo.getDisplayName());
     }
 
     @Test
     void EmptyListOnInitialize() {
-        assertEquals(new ArrayList<Transaction>(), history.getTransactions());
+        assertEquals(new ArrayList<Transaction>(), history.transactions);
+    }
+
+    @Test
+    void addNewTransaction(){
+        history.addTransaction(mockTransaction);
+        assertEquals(mockTransaction, history.transactions.get(0));
     }
 
 }
