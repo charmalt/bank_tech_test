6b845c5651b1d10f2d807d7601cc039db93c9050
Charlene <charlenechetcuti@gmail.com>
2018-06-20 09:40:07 +0100

    refactor print statement

--- src/main/java/bank/PrintStatement.java
+++ src/main/java/bank/PrintStatement.java
@@ -14,14 +14,16 @@ class PrintStatement {
         System.out.printf("%4s || %6s || %5s || %7s\n", "date", "credit", "debit", "balance");
 
         for (Transaction transaction : this.transactions) {
-            printTransaction(transaction);
+            System.out.printf(formatter(transaction), transaction.getDate(), value(transaction), transaction.getBalance());
         }
     }
 
-    private void printTransaction(Transaction transaction){
-        String formatter = (transaction.getWithdrawal() == 0.0) ? "%10s || %4.2f || || %4.2f\n" : "%10s || || %4.2f || %4.2f\n";
-        double value = (transaction.getWithdrawal() == 0.0) ? transaction.getDeposit() : transaction.getWithdrawal();
-        System.out.printf(formatter, transaction.getDate(), value, transaction.getBalance());
+    private String formatter(Transaction transaction){
+        return (transaction.getWithdrawal() == 0.0) ? "%10s || %4.2f || || %4.2f\n" : "%10s || || %4.2f || %4.2f\n";
+    }
+
+    private double value(Transaction transaction){
+        return (transaction.getWithdrawal() == 0.0) ? transaction.getDeposit() : transaction.getWithdrawal();
     }
 
 }
